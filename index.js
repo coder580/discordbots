e4b0d2d801e47b96365b0454f87b24875eae2179
coder580 <ethan.p.schilling@gmail.com>
2019-08-14 00:45:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
-  client.user.setActivity("With my dick", "PLAYING")
+  client.user.setActivity("Nibbers", "WATCHING")
 })
 
 client.on('message', msg => {
