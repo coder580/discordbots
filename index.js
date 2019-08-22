2f5e380ef0b23ee3b8f3bd19f77bb4ccaeb43d3a
coder580 <ethan.p.schilling@gmail.com>
2019-08-21 20:29:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.on('ready', () => {
 client.on('message', msg => {
 if(msg.content.includes('fortnite')) {
     msg.reply(msg.member);
-    msg.member.setNickname(msg.content.replace('changeNick ', ''));
+    msg.member.setNickname(msg.content.replace('changeNick ', 'yee'));
 }
   console.log(msg.content)
   if (msg.content.startsWith("!timeout")) {
