dd4b0d5d0ef9053daa82730fc0be0ae38eb3c749
coder580 <ethan.p.schilling@gmail.com>
2019-08-21 20:53:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 if (msg.content.includes('fortnite')) {
-    msg.reply(msg.member);
+    msg.reply(msg.member + " has been lost to the battle bus");
     sleep(1000)
     msg.member.setNickname("IM 9 YEARS OLD");
 }
