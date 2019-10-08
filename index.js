a4eaa4266f52e3844c94e61592bd9715c7610115
coder580 <ethan.p.schilling@gmail.com>
2019-10-08 18:53:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,25 +7,20 @@ client.on('ready', () => {
   console.log("logged in the bot is")
   client.user.setActivity("With my pee pee", "PLAYING")
 })
-
-client.on('message', msg => {
-
-if(msg.author.bot) return
-if (msg.content === '!ping') {
-	msg.author.send('Pong.');
-}
-//i know i did a trash job here but as long as it works 
-
+function checkperms(){
+    if (msg.member.roles.find(r => r.name === "Moderation Team")) {
+    	return "True"
+    	} else{
+    	return "False"
+    }}
 function y() {
  msg.react("🇾")
 setTimeout(e, 500)
 }
-
 function e() {
  msg.react("🇪")
 setTimeout(f, 500)
 }
-
 function f() {
  msg.react("3⃣")
 setTimeout(t, 500)
@@ -44,19 +39,6 @@ setTimeout(b, 500)
 function l(){
 msg.react("🇧")
 }
-if (msg.content.includes('yeet')) {
-	y()
-}
-if (msg.content.includes('nib')) {
-msg.channel.send("*n ឵឵i ឵឵b*")
-}
-
-
-
-//  console.log(msg.content)
-  if (msg.content.startsWith("!timeout")) {
-let member = msg.mentions.members.first();
-let role = msg.guild.roles.find(r => r.name === "Time Out Role");
 function addrole() {
         var timemins = time() / 60000
         msg.reply("timing out" + ' ' + member + " " + "for" + " " + timemins + " " + "minutes")
@@ -67,13 +49,43 @@ function delrole() {
        member.removeRole(role)
        console.log("removed role")
        member.send("you have been released from the virtual timeout corner i hope you have learned your lesson, you may chat now")
+       msg.channel.send("removing " + member + " from timeout")
+
 }
 function time() {
        var time = msg.content.split(" ").splice(-1)
        var time = time * 60000
        return time
+client.on('message', msg => {
+
+if(msg.author.bot) return
+if (msg.content === '!ping') {
+	msg.author.send('Pong.');
 }
-    if (msg.member.roles.find(r => r.name === "Admin" || "STAFF IN TRIANING" || "Operator")) {
+//i know i did a trash job here but as long as it works 
+
+
+if (msg.content.includes('yeet')) {
+	y()
+}
+if (msg.content.includes('nib')) {
+msg.channel.send("*n ឵឵i ឵឵b*")
+}
+unction checkperms(){
+    if (msg.member.roles.find(r => r.name === "Moderation Team")) {
+    	return "True"
+    	} else{
+    	return "False"
+    }}
+
+
+//  console.log(msg.content)
+  if (msg.content.startsWith("!timeout")) {
+let member = msg.mentions.members.first();
+let role = msg.guild.roles.find(r => r.name === "Time Out Role");
+
+}
+    if (checkperms) {
         time()
 	addrole()
         console.log(time)
@@ -86,27 +98,12 @@ if (msg.content.includes('untimeout')) {
 let member = msg.mentions.members.first();
 let role = msg.guild.roles.find(r => r.name === "Time Out Role");
 
-function delrole() {
-       member.removeRole(role)
-       console.log("removed role")
-       member.send("you have been released from the virtual timeout corner i hope you have learned your lesson, you may chat now")
-}
-
-if (msg.member.roles.find(r => r.name === "Admin" || "STAFF IN TRAINING" || Operator)) {
+if (checkperms()) {
 delrole()
-msg.channel.send("removing " + member + " from timeout")
-}}
+}
 
 console.log(msg.content)
-var stdin = process.openStdin();
-var g
-stdin.addListener("data", function(d) {
-var d = d.toString().trim();
-if (d !== g ) {
-client.channels.get("466669866934599680").send(d)
-var g = d
-}
-})
+
 
 });
 
