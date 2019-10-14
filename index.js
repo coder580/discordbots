221c76eb5dc686af54b17d4f0ae1c90689197b84
coder580 <ethan.p.schilling@gmail.com>
2019-10-13 20:06:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -108,8 +108,8 @@ delrole()
 //logs all chat messages to console, remove if your going to use on a large server for obvious reasons
 console.log(msg.content)
 if (msg.content.startsWith("lol")){
-	client.users.get("279681908793933827").send("your staff application has been looked into\n below is the response");
-	client.users.get("279681908793933827").send({ files: ["./lol.txt"] });
+	client.users.get("448274312085110796").send("your staff application has been looked into\n below is the response");
+	client.users.get("448274312085110796").send({ files: ["./lol.txt"] });
 }
 //end of new message detection
 });
